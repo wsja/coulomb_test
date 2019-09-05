57bd4b917aa2af79220768cc62b830ba302a6a1d
Wasja Bloch <wasja@gfz-potsdam.de>
2019-09-05 14:45:24 +0200

    Exhibit failing tests

---
+++ coulomb.py
@@ -0,0 +1,86 @@
+# Try rotations
+
+import numpy as num
+import math
+
+pi = num.pi
+r2d = 180/pi
+d2r = pi/180
+
+def ned_to_normal_shear_tangential(tensor, strike, dip, rake):
+    """
+    Rotate symmetric (stress or strain) 9-element tensor to coordinate system
+    of the earthquake defined by strike, dip and rake
+    tensor: (float) nn, ne, nd, en, ee, ed, nd, ed, dd
+    strike: (float) stike of the fault [degree]
+    dip: (float) dip of the fault [degree]
+    rake: (float) sense of shearing on the fault plane [degree]
+    """
+
+    def euler_to_matrix(alpha, beta, gamma):
+        '''Given euler angle triplet, create rotation matrix
+
+        Given coordinate system `(x,y,z)` and rotated system `(xs,ys,zs)`
+        the line of nodes is the intersection between the `x,y` and the `xs,ys`
+        planes.
+
+        :param alpha: is the angle between the `z`-axis and the `zs`-axis [rad]
+        :param beta:  is the angle between the `x`-axis and the line of nodes [rad]
+        :param gamma: is the angle between the line of nodes and the `xs`-axis
+            [rad]
+
+        Usage for moment tensors::
+
+            m_unrot = numpy.matrix([[0,0,-1],[0,0,0],[-1,0,0]])
+            euler_to_matrix(dip,strike,-rake, rotmat)
+            m = rotmat.T * m_unrot * rotmat
+
+        '''
+
+        ca = math.cos(alpha)
+        cb = math.cos(beta)
+        cg = math.cos(gamma)
+        sa = math.sin(alpha)
+        sb = math.sin(beta)
+        sg = math.sin(gamma)
+
+        mat = num.matrix([[cb*cg-ca*sb*sg,  sb*cg+ca*cb*sg,  sa*sg],
+                          [-cb*sg-ca*sb*cg, -sb*sg+ca*cb*cg, sa*cg],
+                          [sa*sb,           -sa*cb,          ca]],
+                          dtype=num.float)
+        return mat
+
+    tensor = num.matrix(num.reshape(tensor, (3, 3))).A
+    print(tensor)
+
+    # north-east-down to normal strike rotation matrix
+    rotmat = euler_to_matrix((90+dip)*d2r, -strike*d2r, 90*d2r).A
+    #rotmat = euler_to_matrix((dip + 180.) * d2r, strike * d2r, rake * d2r).A
+    #rotmat = euler_to_matrix(dip*d2r, strike*d2r, -rake*d2r).A
+    #rotmat = euler_to_matrix((dip+180)*d2r, strike*d2r, 0).A
+
+    tensor_nsd = rotmat @ tensor @ rotmat.T  # normal, strike, dip
+    print(tensor_nsd)
+
+    normal = -tensor_nsd[0,0]
+    #slip = -tensor_nsd[0,1]
+    #tang = -tensor_nsd[0,2]
+    
+    #  Shear stress on the fault plane, in strike and dip (positive up)
+    #  coordinates
+    plane_sd = num.matrix([[tensor_nsd[0, 1], 0],
+                           [0, tensor_nsd[0, 2]]]).A
+    print(plane_sd)
+    
+    cr = math.cos(rake*d2r)
+    sr = math.sin(rake*d2r)
+    rotmat_plane = num.matrix([[cr, -sr],
+                               [sr, cr]]).A
+
+    plane_st = rotmat_plane.T @ plane_sd @ rotmat_plane  # slip, tangential
+    print(plane_st)
+    slip = plane_st[0,0]
+    tang = plane_st[1,1]
+    print(normal, slip, tang)
+
+    return normal, slip, tang
---
+++ test_coulomb.py
@@ -0,0 +1,50 @@
+"""
+Tests for the coulomb module
+"""
+
+import unittest
+import coulomb as c
+
+
+class TestCoreFunctions(unittest.TestCase):
+    """ Test the core functions """
+
+    def test_ned_to_normal_shear_tangential(self):
+        """
+        Test rotation to coordinate system of the fault plane
+        """
+                  #  nn ne nd en ee ed dn de dd
+        stresses = [[1, 0, 0, 0, 0, 0, 0, 0, 0],  # 0
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0],  # 1
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0],  # 2
+                    [0, 0, 0, 0, -1, 0, 0, 0, 0], # 3
+                    [0, 0, 0, 0, 1, 0, 0, 0, 0],  # 4
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0],  # 5
+                    [0, 0, 0, 0, -1, 0, 0, 0, 0]] # 6
+
+        str_dip_raks = [[45, 90, 0],   # 0
+                        [0,  90, 0],   # 1
+                        [90, 45, 0],   # 2
+                        [0,  45, 0],   # 3
+                        [0,  45, 90],  # 4
+                        [90, 45, 90],  # 5
+                        [0,  45, -90]] # 6
+
+        nor_shear_tans = [[-0.5, 0.5,  0.0],   # 0
+                          [0.0,  0.0,  0.0],   # 1
+                          [-0.5, 0.0,  0.5],   # 2
+                          [0.5,  0.0, -0.5],   # 3
+                          [-0.5, 0.5,  0],     # 4
+                          [-0.5, 0.5,  0.0],   # 5 
+                          [0.5,  0.5,  0.0]]   # 6 <- fails
+
+        ii = 0
+        for stress, sdr, nst in zip(stresses, str_dip_raks, nor_shear_tans):
+            print(ii)
+            n, s, t = c.ned_to_normal_shear_tangential(
+                stress, sdr[0], sdr[1], sdr[2])
+            self.assertAlmostEqual(n, nst[0], delta=1e-15)
+            self.assertAlmostEqual(s, nst[1], delta=1e-15)
+            self.assertAlmostEqual(t, nst[2], delta=1e-15)
+            ii += 1
+
